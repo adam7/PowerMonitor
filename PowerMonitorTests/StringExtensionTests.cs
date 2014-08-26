421c6c9a999d30b02f073c91542605059ba0e438
Adam Cooper <adam@sequence7.net>
2014-08-26 17:31:17 +0100

    Initial commit

---
+++ PowerMonitorTests/StringExtensionTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PowerMonitor.Extensions;
+
+namespace PowerMonitorTests
+{
+    [TestClass]
+    public class StringExtensionTests
+    {
+        [TestMethod]
+        public void WhenStringIsNull_NullShouldBeReturned()
+        {
+            string text = null;
+
+            Assert.AreEqual(text.Tail(5), null);
+        }
+
+        [TestMethod]
+        public void WhenStringIsEmpty_EmptyStringShouldBeReturned()
+        {
+            var text = string.Empty;
+
+            Assert.AreEqual(text.Tail(5), string.Empty);
+        }
+
+
+        [TestMethod]
+        public void WhenStringIsShorterThanLength_StringShouldBeReturned()
+        {
+            var text = "1";
+
+            Assert.AreEqual(text.Tail(5), text);
+        }
+
+        [TestMethod]
+        public void WhenStringIsLongerThanLength_SubstringShouldBeReturned()
+        {
+            var text = "abcde12345";
+
+            Assert.AreEqual(text.Tail(5), "12345");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+PowerMonitor
+============
+
+PowerMonitor is a sample application written using ASP.NET Web Api + Angular + Chart.js + Bootstrap 
+
+[![Build status](https://ci.appveyor.com/api/projects/status/j4wpiw02ouvj9pp4)](https://ci.appveyor.com/project/adam7/powermonitor)
